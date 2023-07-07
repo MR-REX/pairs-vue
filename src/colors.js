624ecfba12ce0c1357c02d57c4a9e2c599bf87c8
MR-REX <I-MR-REX@yandex.ru>
2023-07-08 02:22:51 +0300

    New colors

--- src/colors.js
+++ src/colors.js
@@ -4,5 +4,8 @@ export default {
     violet: '#9b59b6',
     yellow: '#f1c40f',
     orange: '#e67e22',
-    red: '#e74c3c'
+    red: '#e74c3c',
+    pink: '#fd79a8',
+    grey: '#95a5a6',
+    emerald: '#1abc9c'
 }
